e03e28c2d84b645235a45551dd4f5fa2a69c68f5
Adam Yala <adamkyala@gmail.com>
2023-05-16 19:00:28 -0600

    add pre-commit. small refactor

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import time
 from datetime import datetime
+
+import numpy
 import pyautogui
 from PIL import Image
-import time
-import numpy
 
 DEBUG = False
-
-bobber_image_file = Image.open('zone_images/booty-bay.png')
-ERROR_SENSITIVEITY = 1.4
+ERROR_SENSITIVITY = 1.4
+LOCATION = "booty-bay"
+LOCATOR_CONFIDENCE = 0.35
 
 
 def get_cropped_screen(left, top, right, bottom):
@@ -16,9 +17,9 @@ def get_cropped_screen(left, top, right, bottom):
     cropped_image = image.crop((left, top, right, bottom))
 
     if DEBUG:
-        cropped_image.save('cropped-image.png')
+        cropped_image.save("cropped-image.png")
 
-    print('cropped image size', cropped_image.size)
+    print("cropped image size", cropped_image.size)
 
     return cropped_image
 
@@ -31,7 +32,7 @@ def get_bobber_center(left, top, target):
     true_center_x = center_x + left
     true_center_y = center_y + top
 
-    print('bobber coords', true_center_x, true_center_y)
+    print("bobber coords", true_center_x, true_center_y)
 
     return true_center_x, true_center_y
 
@@ -42,42 +43,39 @@ def watch_bobber(x, y):
     initial_bobber = pyautogui.screenshot(region=bobber_region)
 
     if DEBUG:
-        initial_bobber.save('initial-bobber.png')
+        initial_bobber.save("initial-bobber.png")
 
     initial_array = numpy.asarray(initial_bobber)
     count = 1
     threshold = 0
 
     while time.time() < t_end:
-        print('watch', count)
+        print("watch", count)
 
         watched_bobber = pyautogui.screenshot(region=bobber_region)
 
         if DEBUG:
-            watched_bobber.save('watched-bobber.png')
+            watched_bobber.save("watched-bobber.png")
 
         watched_array = numpy.asarray(watched_bobber)
-        image_diff = (initial_array.astype("float") - watched_array.astype("float"))
-        err = numpy.sum(image_diff ** 2)
+        image_diff = initial_array.astype("float") - watched_array.astype("float")
+        err = numpy.sum(image_diff**2)
         err /= float(initial_array.shape[0] * initial_array.shape[1])
 
         if not threshold:
-            threshold = err * ERROR_SENSITIVEITY
+            threshold = err * ERROR_SENSITIVITY
 
-        print('image_diff', err)
+        print("image_diff", err)
         count += 1
 
         if err > threshold:
-            print('bobber moved')
+            print("bobber moved")
             return True
-
-    # todo: comment out
-    # bobber.save('bobber.png')
 
 
 def main():
     screen = pyautogui.screenshot()
-    print('screen size', screen.size)
+    print("screen size", screen.size)
     # -------------
     # | | | | | | |
     # -------------
@@ -88,39 +86,44 @@ def main():
     # | | | | | | |
     # -------------
     width, height = screen.size
-    left = width * (1/3)
-    top = height * (1/2)
-    right = width * (2/3)
-    bottom = height * (3/4)
+    left = width * (1 / 3)
+    top = height * (1 / 2)
+    right = width * (2 / 3)
+    bottom = height * (3 / 4)
 
     time.sleep(3)
 
     while True:
         pyautogui.moveTo(200, 200)
-        print('resetting cursor', datetime.now())
+        print("resetting cursor", datetime.now())
 
         # run macro to cast bobber
-        pyautogui.press('=')
-        print('casting bobber')
+        pyautogui.press("=")
+        print("casting bobber")
         time.sleep(2)
 
         cropped_image = get_cropped_screen(left, top, right, bottom)
 
+        bobber_image_file = Image.open(f"zone_images/{LOCATION}.png")
+
         # look for the bobber. some zones work better with a different confidence level than others
-        target = pyautogui.locate(bobber_image_file, cropped_image, confidence=.35)
+        target = pyautogui.locate(
+            bobber_image_file, cropped_image, confidence=LOCATOR_CONFIDENCE
+        )
         if not target:
-            print('not found')
-            return
+            print("not found")
+            continue
 
         bobber_x, bobber_y = get_bobber_center(left, top, target)
 
-        # TODO: comment out
-        # pyautogui.moveTo(x=bobber_x / 2, y=bobber_y / 2)
-        print('found it')
+        if DEBUG:
+            pyautogui.moveTo(x=bobber_x / 2, y=bobber_y / 2)
+        print("found it")
 
         bite = watch_bobber(bobber_x, bobber_y)
         if bite:
             pyautogui.rightClick(x=bobber_x / 2, y=bobber_y / 2)
             time.sleep(1)
+
 
 main()
--- wow-fishing.py
+++ wow-fishing.py
@@ -1,14 +1,14 @@
-import os
 import glob
+import os
+import time
 from datetime import datetime, timedelta
+
 import numpy
 import pyautogui
-import time
 from PIL import Image
 
-
 # the zone we're fishing in
-ZONE = 'tanaris'
+ZONE = "durotar"
 # the number of screenshots to take of the still bobber
 # these images build a standard deviation to compare again
 BOBBER_SAMPLE_SIZE = 10
@@ -20,146 +20,146 @@ pyautogui.PAUSE = 2
 time.sleep(5)
 
 zone_mapping = {
-    'arathi-highlands': {
-        'image': 'arathi-highlands.png',
-        'confidence': 0.4,
-        'standard-deviations': 3,
-        'open-clams': False,
+    "arathi-highlands": {
+        "image": "arathi-highlands.png",
+        "confidence": 0.4,
+        "standard-deviations": 3,
+        "open-clams": False,
     },
-    'azshara': {
-        'image': 'azshara.png',
-        'confidence': 0.5,
-        'standard-deviations': 2,
-        'open-clams': True,
+    "azshara": {
+        "image": "azshara.png",
+        "confidence": 0.5,
+        "standard-deviations": 2,
+        "open-clams": True,
     },
-    'barrens': {
-        'image': 'barrens.png',
-        'confidence': 0.5,
-        'standard-deviations': 2.5,
-        'open-clams': False,
+    "barrens": {
+        "image": "barrens.png",
+        "confidence": 0.5,
+        "standard-deviations": 2.5,
+        "open-clams": False,
     },
-    'durotar': {
-        'image': 'durotar.png',
-        'confidence': 0.3,
-        'standard-deviations': 2.5,
-        'open-clams': False,
+    "durotar": {
+        "image": "durotar.png",
+        "confidence": 0.3,
+        "standard-deviations": 2.5,
+        "open-clams": False,
     },
-    'dustwallow-marsh': {
-        'image': 'dustwallow-marsh.png',
-        'confidence': 0.4,
-        'standard-deviations': 3,
-        'open-clams': False,
+    "dustwallow-marsh": {
+        "image": "dustwallow-marsh.png",
+        "confidence": 0.4,
+        "standard-deviations": 3,
+        "open-clams": False,
     },
-    'eastern_plaguelands': {
-        'image': 'eastern_plaguelands.png',
-        'confidence': 0.5,
-        'standard-deviations': 3,
-        'open-clams': False,
+    "eastern_plaguelands": {
+        "image": "eastern_plaguelands.png",
+        "confidence": 0.5,
+        "standard-deviations": 3,
+        "open-clams": False,
     },
-    'elwynn-forest': {
-        'image': 'elwynn-forest.png',
-        'confidence': 0.4,
-        'standard-deviations': 3,
-        'open-clams': False,
+    "elwynn-forest": {
+        "image": "elwynn-forest.png",
+        "confidence": 0.4,
+        "standard-deviations": 3,
+        "open-clams": False,
     },
-    'feralas': {
-        'image': 'feralas.png',
-        'confidence': 0.5,
-        'standard-deviations': 2,
-        'open-clams': True,
+    "feralas": {
+        "image": "feralas.png",
+        "confidence": 0.5,
+        "standard-deviations": 2,
+        "open-clams": True,
     },
-    'moonglade': {
-        'image': 'moonglade.png',
-        'confidence': 0.5,
-        'standard-deviations': 2.5,
-        'open-clams': False,
+    "moonglade": {
+        "image": "moonglade.png",
+        "confidence": 0.5,
+        "standard-deviations": 2.5,
+        "open-clams": False,
     },
-    'redridge-mountains': {
-        'image': 'redridge-mountains.png',
-        'confidence': 0.4,
-        'standard-deviations': 3.5,
-        'open-clams': False,
+    "redridge-mountains": {
+        "image": "redridge-mountains.png",
+        "confidence": 0.4,
+        "standard-deviations": 3.5,
+        "open-clams": False,
     },
-    'southshore': {
-        'image': 'southshore.png',
-        'confidence': 0.4,
-        'standard-deviations': 3.25,
-        'open-clams': False,
+    "southshore": {
+        "image": "southshore.png",
+        "confidence": 0.4,
+        "standard-deviations": 3.25,
+        "open-clams": False,
     },
-    'stormwind': {
-        'image': 'stormwind.png',
-        'confidence': 0.4,
-        'standard-deviations': 3.5,
-        'open-clams': False,
+    "stormwind": {
+        "image": "stormwind.png",
+        "confidence": 0.4,
+        "standard-deviations": 3.5,
+        "open-clams": False,
     },
-    'stv': {
-        'image': 'stv.png',
-        'confidence': 0.5,
-        'standard-deviations': 3.5,
-        'open-clams': False,
+    "stv": {
+        "image": "stv.png",
+        "confidence": 0.5,
+        "standard-deviations": 3.5,
+        "open-clams": False,
     },
-    'tanaris': {
-        'image': 'tanaris.png',
-        'confidence': 0.4,
-        'standard-deviations': 5.5,
-        'open-clams': True,
+    "tanaris": {
+        "image": "tanaris.png",
+        "confidence": 0.4,
+        "standard-deviations": 5.5,
+        "open-clams": True,
     },
-    'wetlands': {
-        'image': 'wetlands.png',
-        'confidence': 0.5,
-        'standard-deviations': 3,
-        'open-clams': False,
+    "wetlands": {
+        "image": "wetlands.png",
+        "confidence": 0.5,
+        "standard-deviations": 3,
+        "open-clams": False,
     },
-    'winterspring': {
-        'image': 'winterspring.png',
-        'confidence': 0.4,
-        'standard-deviations': 2,
-        'open-clams': False,
+    "winterspring": {
+        "image": "winterspring.png",
+        "confidence": 0.4,
+        "standard-deviations": 2,
+        "open-clams": False,
     },
 }
 
 IMAGE_FILE = f'zone_images/{zone_mapping[ZONE]["image"]}'
 CONFIDENCE = zone_mapping[ZONE]["confidence"]
 STANDARD_DEVIATIONS = zone_mapping[ZONE]["standard-deviations"]
-OPEN_CLAMS = zone_mapping[ZONE]['open-clams']
+OPEN_CLAMS = zone_mapping[ZONE]["open-clams"]
 
 
 def reset_cursor():
     # puts the cursor in the upper left of the screen
     pyautogui.moveTo(200, 200)
-    print('resetting cursor', datetime.now())
+    print("resetting cursor", datetime.now())
 
 
 # different actions are performed 10 min after fishing start
 # get the fishing start time so we can determine when to perform actions
-print('fishing in ', ZONE)
+print("fishing in ", ZONE)
 start_fishing = datetime.utcnow()
 while True:
     if datetime.utcnow() - start_fishing > timedelta(minutes=10):
         if ADD_BAIT:
             # run macro to add bait
-            pyautogui.press('0')
+            pyautogui.press("0")
             time.sleep(10)
-            print('bait applied')
+            print("bait applied")
         if OPEN_CLAMS:
             # spam macro to open clams and free up inventory space
             for _ in range(10):
-                pyautogui.press('-')
+                pyautogui.press("-")
         start_fishing = datetime.utcnow()
 
     # run macro to cast bobber
-    pyautogui.press('=')
+    pyautogui.press("=")
     time.sleep(2)
-    print('')
-    print('casting bobber')
+    print("")
+    print("casting bobber")
 
     # take screenshot
-    pyautogui.screenshot('screenshot.png')
+    pyautogui.screenshot("screenshot.png")
 
     # the bobber most often lands in the top center of the screen
     # crop the screenshot so we can search only that area
     # faster than searching the whole screen
-    with Image.open('screenshot.png') as original_file:
+    with Image.open("screenshot.png") as original_file:
         # -------------
         # | | | | | | |
         # -------------
@@ -177,15 +177,15 @@ while True:
         right = (width / 3) * 2
         bottom = height / 2
         cropped_image = original_file.crop((left, top, right, bottom))
-    cropped_image.save('cropped.png')
+    cropped_image.save("cropped.png")
 
     # look for the bobber. some zones work better with a different confidence level than others
     target = pyautogui.locate(IMAGE_FILE, cropped_image, confidence=CONFIDENCE)
 
     if target is None:
-        print('not found', datetime.now())
+        print("not found", datetime.now())
         continue
-    print('found it', datetime.now())
+    print("found it", datetime.now())
 
     target_center = pyautogui.center(target)
     center_x, center_y = target_center
@@ -199,7 +199,7 @@ while True:
     now = start
 
     # clean up old bobber monitoring images if they exist
-    monitor_images = glob.glob('monitor_bobber_*.png')
+    monitor_images = glob.glob("monitor_bobber_*.png")
     for image in monitor_images:
         os.remove(image)
     bobber_image_averages = []
@@ -210,11 +210,13 @@ while True:
         # full bobber into the screenshot
         monitor_left = offset_center_x - 40
         monitor_top = offset_center_y - 40
-        bobber = pyautogui.screenshot(f'monitor_bobber_{now}.png', region=(monitor_left, monitor_top, 100, 100))
+        bobber = pyautogui.screenshot(
+            f"monitor_bobber_{now}.png", region=(monitor_left, monitor_top, 100, 100)
+        )
 
         # get the avg value of the monitor image
         bobber_average = numpy.average(bobber)
-        print('monitor avg', bobber_average, datetime.now())
+        print("monitor avg", bobber_average, datetime.now())
 
         # collect some monitor image avgs for a good baseline value
         if len(bobber_image_averages) < BOBBER_SAMPLE_SIZE:
@@ -222,11 +224,13 @@ while True:
             continue
 
         bobbed_averages = numpy.average(bobber_image_averages)
-        comparison_bobber = bobbed_averages + (STANDARD_DEVIATIONS * numpy.std(bobber_image_averages))
-        print('comp bobber', comparison_bobber, datetime.now())
+        comparison_bobber = bobbed_averages + (
+            STANDARD_DEVIATIONS * numpy.std(bobber_image_averages)
+        )
+        print("comp bobber", comparison_bobber, datetime.now())
 
         if bobber_average > comparison_bobber:
-            print('a bite!', datetime.now())
+            print("a bite!", datetime.now())
             pyautogui.rightClick(monitor_left + 40, monitor_top + 40)
             reset_cursor()
             break
